4a68941a42364b6dd1c4ed52bead64374443d3a4
dvaJi <francisco_p@outlook.com>
2016-03-21 21:40:40 -0300

    Added files via upload

---
+++ assets/themes/admin/js/admin.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+
+	var container = document.getElementById('slide-out');
+	Ps.initialize(container);
+
+    $('#data-table-simple').DataTable({
+        "order": [[ 1, "desc" ]]
+    } )
+
+
+    	
+
+
+} );
